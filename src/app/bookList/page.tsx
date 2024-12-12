fddb0318468cc0b0908dda58133299496dbfd29e
glaryull <90367250+Glaryull@users.noreply.github.com>
2024-12-12 17:52:45 +0900

    중간점검5:52

--- src/app/bookList/page.tsx
+++ src/app/bookList/page.tsx
@@ -1,28 +1,21 @@
 import React from "react";
 import Pagination from "./_view/Pagination";
 
+// const TABS = [{ labelName: "전체", value: "all" },
+//     {labelName:''}
+// ];
 type Props = {
-  params: {};
+  params: { bookId: string; bookName: string; author: string };
+  serchParams: {};
 };
 
 export default async function bookListPage({}: Props) {
-  function pageChangeHandler(page: number): void {
-    throw new Error("Function not implemented.");
-  }
-
   return (
     <div className="container">
       <h1 className="overflow-hidden text-gray-700 font-bold items-center text-center">
         책 목록을 검색할 수 있어요. 제목과 저자 별로 검색을 할 수 있어요.
       </h1>
       <section></section>
-      <Pagination
-        currentPage={1}
-        totalPages={10}
-        pageGroupSize={24}
-        onPageChange={pageChangeHandler}
-        children={undefined}
-      />
     </div>
   );
 }
--- src/requests/bookList/fetchShowBookList.tsx
+++ src/requests/bookList/fetchShowBookList.tsx
@@ -1,18 +1,51 @@
-
-type Props = {
+type ListProps = {
   bookId: string;
-
 };
 
-type Response = {
+type ListResponse = {
   bookId: string;
   bookName: string;
   author: string;
   publisher: string;
-  price: number;
-  bookType: ["국내도서" | "외국도서" | "eBook"];
-
+  price: string;
+  genre:
+    | "novel"
+    | "essay"
+    | "science"
+    | "science fiction"
+    | "megazine"
+    | undefined;
+  bookType: "국내도서" | "외국도서" | "eBook";
+  sellingQuantity: number;
+  inventoryQuantity: number;
+  orderQuantity: number;
 };
-const MOCK_DATA={
-    bookId:
+const MOCK_DATA = [
+  {
+    bookId: "1",
+    bookName: "소년이 온다",
+    author: "한강",
+    publisher: "창비",
+    price: "13,500",
+    genre: undefined,
+    bookType: "국내도서",
+    sellingQuantity: 100000,
+    inventoryQuantity: 1000,
+    orderQuantity: 5000,
+  },
+  {
+    bookId: "2",
+    bookName: "지구 끝의 온실",
+    author: "김초엽",
+    publisher: "창비",
+    price: "18000",
+    genre: "science Fiction",
+    bookType: "국내도서",
+    sellingQuantity: 40000,
+    inventoryQuantity: 100,
+    orderQuantity: 3000,
+  },
+];
+export default async function fetchShowBookList({ bookId }: ListProps) {
+  return { data: MOCK_DATA as ListResponse[] };
 }
