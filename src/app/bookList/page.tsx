5b5675790cb8cc24101d2f74c1d924fb2e0eeb4f
glaryull <90367250+Glaryull@users.noreply.github.com>
2024-12-12 17:15:47 +0900

    중간점검

--- src/app/bookList/page.tsx
+++ src/app/bookList/page.tsx
@@ -1,13 +1,28 @@
 import React from "react";
+import Pagination from "./_view/Pagination";
 
-type Props = {};
+type Props = {
+  params: {};
+};
 
-export default function bookListPage({}: Props) {
+export default async function bookListPage({}: Props) {
+  function pageChangeHandler(page: number): void {
+    throw new Error("Function not implemented.");
+  }
+
   return (
-    <div>
+    <div className="container">
       <h1 className="overflow-hidden text-gray-700 font-bold items-center text-center">
         책 목록을 검색할 수 있어요. 제목과 저자 별로 검색을 할 수 있어요.
       </h1>
+      <section></section>
+      <Pagination
+        currentPage={1}
+        totalPages={10}
+        pageGroupSize={24}
+        onPageChange={pageChangeHandler}
+        children={undefined}
+      />
     </div>
   );
 }
---
+++ src/requests/bookList/fetchShowBookList.tsx
@@ -0,0 +1,18 @@
+
+type Props = {
+  bookId: string;
+
+};
+
+type Response = {
+  bookId: string;
+  bookName: string;
+  author: string;
+  publisher: string;
+  price: number;
+  bookType: ["국내도서" | "외국도서" | "eBook"];
+
+};
+const MOCK_DATA={
+    bookId:
+}
